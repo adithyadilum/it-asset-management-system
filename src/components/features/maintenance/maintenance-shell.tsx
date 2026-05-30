8441de3b58b9952419df8b18ce42f992069c542c
Tharushi Hasinika <tharushiliyanage029@gmail.com>
2026-05-30 21:41:09 +0530

    style(maintenance): remove custom manual sliding div container from maintenance shell

--- src/components/features/maintenance/maintenance-shell.tsx
+++ src/components/features/maintenance/maintenance-shell.tsx
@@ -168,23 +168,12 @@ export function MaintenanceShell() {
         </main>
       </div>
 
-      <div
-        className={cn(
-          "shrink-0 bg-background overflow-hidden transition-all duration-300 ease-in-out transform",
-          uiState.isPanelOpen
-            ? "w-137.5 xl:w-150 border-l border-border opacity-100 translate-x-0"
-            : "w-0 border-0 opacity-0 translate-x-8"
-        )}
-      >
-        <div className="w-137.5 xl:w-150 h-full flex flex-col">
-          <IssueReviewPanelWrapper
-            isOpen={uiState.isPanelOpen}
-            onClose={handlePanelClose}
-            ticketId={uiState.selectedTicketId}
-            onSuccess={() => loadData(debouncedSearch)}
-          />
-        </div>
-      </div>
+      <IssueReviewPanelWrapper
+        isOpen={uiState.isPanelOpen}
+        onClose={handlePanelClose}
+        ticketId={uiState.selectedTicketId}
+        onSuccess={() => loadData(debouncedSearch)}
+      />
 
       <LogCompleteRepairDialog
         isOpen={uiState.showCompleteDialog}
